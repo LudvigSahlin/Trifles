689dc1ae3369118b12d722cb1e2e2826e0dfafba
Ludvig Sahlin <ludvigsahlin@hotmail.com>
2021-07-22 09:06:32 +0200

    Add UIView extension functions for changing the layer.

---
+++ Sources/Trifles/UIView+Layer.swift
@@ -0,0 +1,27 @@
+//
+//  UIView+Layer.swift
+//  
+//
+//  Created by Ludvig Hemma on 2021-07-21.
+//
+
+import Foundation
+import UIKit
+
+public extension UIView {
+    func applyBorders(color: UIColor, borderWidth: CGFloat = 1, cornerRadius: CGFloat = 4) {
+        layer.borderColor = color.cgColor
+        layer.borderWidth = borderWidth
+        layer.cornerRadius = cornerRadius
+    }
+    func applyShadow(opacity: Float = 0.4, offsetX: CGFloat = 0, offsetY: CGFloat = 2, shadowRadius: CGFloat = 2) {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowOffset = CGSize(width: offsetX, height: offsetY)
+        layer.shadowRadius = shadowRadius
+    }
+    func applyCardTraits(cornerRadius: CGFloat = 4) {
+        layer.cornerRadius = cornerRadius
+        applyShadow()
+    }
+}
