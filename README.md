09b7d5c3cfb22822abb3ff92f772a4dc6b29884d
Ludvig Sahlin <ludvigsahlin@hotmail.com>
2021-09-03 11:08:39 +0200

    Update README.md and add license
    - Rename dismissKeyboardOnTap.

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
 # Trifles
 
-A description of this package.
+This package is packed with small useful extension functions.
+It also contains helper functions defined in Trifles.swift.
+
+## Extension functions
+Array:
+- isNeitherNilNorEmpty
+
+Date:
+- todayMidnight
+- tomorrowMidnight
+- yesterdayMidnight
+
+Optional:
+- isNeitherNilNorEmpty
+
+UIView:
+- applyBorders
+- applyShadow
+- applyCardTraits
+
+UIViewController:
+- dismissKeyboardOnTap
+
+## Other helper functions
+Trifles:
+- TextField.build // Adds subviews and sets basic styling. Sets no constraints.
+- Button.build    // Adds subviews and sets basic styling. Sets no constraints.
+
+
+
+# Usage:
+
+Most functions should be self explanatory. Here are two examples:
+
+    let (containerView, backgroundView, label) = Trifles.Button.build()
+    containerView.applyBorders()
+
+And if the keyboard should be dismissed when tapping:
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dismissKeyboardOnTap()
+    }
+
+
+
+# License
+
+The contents of this repository is licensed under the
+[Apache License, version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
+
+
+
+# Contact
+
+ludvigsahlin2@gmail.com
--- Sources/Trifles/Trifles.swift
+++ Sources/Trifles/Trifles.swift
@@ -1,10 +1,24 @@
+// Copyright 2021 Ludvig Sahlin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import UIKit
 
 public class Trifles {
 
     public class TextField {
 
-        /// Adds subviews, delegate and basic styling. Sets no constraints.
+        /// Adds subviews and sets basic styling. Sets no constraints.
         public static func build() -> (containerView: UIView, backgroundView: UIView, textField: UITextField, headerLabel: UILabel, xImageView: UIImageView, xBackgroundView: UIView) {
             let containerView = UIView()
 
@@ -41,7 +55,7 @@ public class Trifles {
 
     public class Button {
 
-        /// Adds subviews and basic styling. Sets no constraints.
+        /// Adds subviews and sets basic styling. Sets no constraints.
         public static func build() -> (containerView: UIView, backgroundView: UIView, label: UILabel) {
             let containerView = UIView()
 
